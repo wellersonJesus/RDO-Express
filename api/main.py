4127cbf7672135958ebceb1428a3826d06fcaacf
wellersonjesus <wellersonjesus@hotmail.com>
2026-05-04 13:54:11 -0300

    fix: criando estrutura da pagina de usuarios 💭...

--- api/main.py
+++ api/main.py
@@ -9,6 +9,15 @@ app.add_middleware(CORSMiddleware, allow_origins=["*"], allow_methods=["*"], all
 def read_root():
     return {"status": "RDO Express API Online", "version": "1.0.0"}
 
+# Endpoint temporário para teste de consumo de dados
+@app.get("/usuarios")
+def get_usuarios():
+    # Aqui você conectará futuramente com a Planilha via App Script
+    return [
+        {"id": 1, "nome": "Wellerson"},
+        {"id": 2, "nome": "Rodrigo Pereira"}
+    ]
+
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run(app, host="0.0.0.0", port=8000)
